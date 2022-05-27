16f6269f2f97d54575c96abbe4d43e1bee8bf370
JoseFreitass <85848930+JoseFreitass@users.noreply.github.com>
2022-05-26 22:20:55 -0300

    ;)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import styles from  '../styles/Home.module.css'
 
 
+
+
 const Home: NextPage = () => {
   return (
       <main>
@@ -17,20 +19,34 @@ const Home: NextPage = () => {
             <div className={styles.subcontainer1}>
 
             
-              <div className={styles.container1}>
-                <button>1</button>
+              <div className={styles.container11}>
+                <div className={styles.btnestiliza1}></div>
+                <div className={styles.titulo}>CAIXA ELETRÔNICO</div>
               </div>
 
-              <div className={styles.container1}>
-                <button>2</button>
+              <div className={styles.container11}>
+                <button className={styles.btnestiliza1}></button>
+                <button className={styles.btnestiliza}>Saque</button>
               </div>
 
-              <div className={styles.container1}>
-                <button>3</button>
+              <div className={styles.container11}>
+                <button className={styles.btnestiliza1}></button>
+                <button className={styles.btnestiliza}>Consultar Saldo</button>
               </div>
 
-              <div className={styles.container1}>
-                <button>4</button>
+              <div className={styles.container11}>
+                <button className={styles.btnestiliza1}></button>
+                <button className={styles.btnestiliza}>Extrato Financeiro</button>
+              </div>
+
+              <div className={styles.container11}>
+                <button className={styles.btnestiliza1}></button>
+                <button className={styles.btnestiliza}>Transferência</button>
+              </div>
+
+              <div className={styles.container11}>
+                <div className={styles.btnestiliza1}></div> 
+                <div className={styles.subtitulo}>Central de Ajuda</div>
               </div>
 
             </div>
@@ -41,21 +57,32 @@ const Home: NextPage = () => {
             <div className={styles.subcontainer2}>
 
             
-              <div className={styles.subcontainer2}>
-                <button>1</button>
-              </div>
+            <div className={styles.container11}>
+                <div className={styles.btnestiliza1}></div>
+                <div className={styles.btnestiliza1}></div>  
+            </div>
+            <div className={styles.container11}>
+                <button className={styles.btnestiliza}>Contratar Planos</button>
+                <button className={styles.btnestiliza1}></button>  
+            </div>
+            <div className={styles.container11}>
+                <button className={styles.btnestiliza}>Cheque Especial</button>
+                <button className={styles.btnestiliza1}></button>  
+            </div>
+            <div className={styles.container11}>
+                <button className={styles.btnestiliza}>Cancelamento de Cartão</button>
+                <button className={styles.btnestiliza1}></button>  
+            </div>
+            <div className={styles.container11}>
+                <button className={styles.btnestiliza}>Dúvidas</button>
+                <button className={styles.btnestiliza1}></button>  
+            </div>
+            <div className={styles.container11}>
+                <div className={styles.subtitulo}>0800 - 33599853</div>
+                <div className={styles.btnestiliza1}></div>   
+            </div>
 
-              <div className={styles.subcontainer2}>
-                <button>2</button>
-              </div>
-
-              <div className={styles.subcontainer2}>
-                <button>3</button>
-              </div>
-
-              <div className={styles.container1}>
-                <button>4</button>
-              </div>
+              
               
             </div>
 
